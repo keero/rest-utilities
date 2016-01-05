637c49078ef2db0a480d34c4d10a8d270ba837f6
Martin Kero <martin.kero@neonstingray.com>
2016-01-05 13:40:45 +0100

    Implemented JSON parsing for List.

--- ready4air/dto/List.h
+++ ready4air/dto/List.h
@@ -122,7 +122,101 @@ namespace ready4air
 
         virtual bool InitFromJsonValue(const rapidjson::Value &value)
         {
-            return false;
+            {
+                // Mandatory property
+                if (!value.HasMember("ListId") || !value["ListId"].IsInt()) return false;
+                SetListId(value["ListId"].GetInt());
+            }
+            {
+                // Mandatory property
+                if (!value.HasMember("ListType") || !value["ListType"].IsInt()) return false;
+                SetListType(value["ListType"].GetInt());
+            }
+            {
+                // Mandatory property
+                if (!value.HasMember("Name") || !value["Name"].IsString()) return false;
+                SetName(value["Name"].GetString());
+            }
+            {
+                // Mandatory property
+                if (!value.HasMember("IsGenre") || !value["IsGenre"].IsBool()) return false;
+                SetIsGenre(value["IsGenre"].GetBool());
+            }
+            {
+                // Non-mandatory property
+                if (value.HasMember("Description"))
+                {
+                    if (!value["Description"].IsString()) return false;
+                    SetDescription(value["Description"].GetString());
+                }
+            }
+            {
+                // Non-mandatory property
+                if (value.HasMember("Images"))
+                {
+                    if (!value["Images"].IsArray()) return false;
+                    std::vector<Image> images;
+                    for (rapidjson::SizeType i = 0; i < value["Images"].Size(); i += 1)
+                    {
+                        Image image;
+                        if (!value["Images"][i].IsObject() || !image.InitFromJsonValue(value["Images"][i])) return false;
+                        images.push_back(image);
+                    }
+                    SetImages(images);
+                }
+            }
+            {
+                // Non-mandatory property
+                if (value.HasMember("Custom"))
+                {
+                    if (!value["Custom"].IsObject()) return false;
+                    std::map<std::string, std::string> custom;
+                    for (rapidjson::Value::ConstMemberIterator itr = value.MemberBegin(); itr != value.MemberEnd(); ++itr)
+                    {
+                        if (itr->value.GetType() != rapidjson::kStringType) return false;
+                        custom[itr->name.GetString()] = itr->value.GetString();
+                    }
+                    SetCustom(custom);
+                }
+            }
+            {
+                // Non-mandatory property
+                if (value.HasMember("LanguageLists"))
+                {
+                    if (!value["LanguageLists"].IsArray()) return false;
+                    std::vector<LanguageList> languageLists;
+                    for (rapidjson::SizeType i = 0; i < value["LanguageLists"].Size(); i += 1)
+                    {
+                        LanguageList languageList;
+                        if (!value["LanguageLists"][i].IsObject() || !languageList.InitFromJsonValue(value["LanguageLists"][i])) return false;
+                        languageLists.push_back(languageList);
+                    }
+                    SetLanguageLists(languageLists);
+                }
+            }
+            {
+                // Non-mandatory property
+                if (value.HasMember("ListMedias"))
+                {
+                    if (!value["ListMedias"].IsArray()) return false;
+                    std::vector<ListMedia> listMedias;
+                    for (rapidjson::SizeType i = 0; i < value["ListMedias"].Size(); i += 1)
+                    {
+                        ListMedia listMedia;
+                        if (!value["ListMedias"][i].IsObject() || !listMedia.InitFromJsonValue(value["ListMedias"][i])) return false;
+                        listMedias.push_back(listMedia);
+                    }
+                    SetListMedias(listMedias);
+                }
+            }
+            {
+                // Mandatory property
+                Link items;
+                if (!value.HasMember("Items") || !value["Items"].IsObject() || !items.InitFromJsonValue(value["Items"])) return false;
+                SetItems(items);
+            }
+
+            return true;
         }
 
     private:
