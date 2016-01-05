31b78e9d758014c1bcb66fa9b887323be9499c14
Martin Kero <martin.kero@neonstingray.com>
2016-01-05 10:01:08 +0100

    Implemented JSON parsing for Genre.

--- ready4air/dto/Genre.h
+++ ready4air/dto/Genre.h
@@ -59,6 +59,34 @@ namespace ready4air
 
         virtual bool InitFromJsonValue(const rapidjson::Value &value)
         {
+            {
+                // Non-mandatory property
+                if (value.HasMember("Name"))
+                {
+                    if (!value["Name"].IsString()) return false;
+                    SetName(value["Name"].GetString());
+                }
+            }
+            {
+                // Non-mandatory property
+                if (value.HasMember("Link"))
+                {
+                    Link link;
+                    if (!value["Link"].IsObject() || !link.InitFromJsonValue(value["Link"])) return false;
+                    SetLink(link);
+                }
+            }
+            {
+                // Mandatory property
+                if (!value.HasMember("IsMain") || !value["IsMain"].IsBool()) return false;
+                SetIsMain(value["IsMain"].GetBool());
+            }
+            {
+                // Mandatory property
+                if (!value.HasMember("ListId") || !value["ListId"].IsInt()) return false;
+                SetListId(value["ListId"].GetInt());
+            }
+
             return false;
         }
 
