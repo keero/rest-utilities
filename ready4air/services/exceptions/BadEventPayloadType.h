c801307a523c83d7ba174b57c7718536328c07ce
Martin Kero <martin.kero@neonstingray.com>
2016-01-27 16:38:25 +0100

    Added exception for bad event payload data type.

---
+++ ready4air/services/exceptions/BadEventPayloadType.h
@@ -0,0 +1,17 @@
+#ifndef READY4AIR_SERVICES_BADEVENTPAYLOADTYPE_H
+#define READY4AIR_SERVICES_BADEVENTPAYLOADTYPE_H
+
+#include <exception>
+
+namespace ready4air
+{
+    class BadEventPayloadType : public std::exception
+    {
+        virtual const char *what() const throw()
+        {
+            return "Template class EVENT_PAYLOAD_TYPE must implement ready4air::IEventPayload";
+        }
+    };
+}
+
+#endif //READY4AIR_SERVICES_BADEVENTPAYLOADTYPE_H
